51550d1c53f6fa092195688eaa9a9615b42f8e93
unknown <wilcleyber2@gmail.com>
2026-02-13 06:00:35 -0300

    Configurações correta para github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Document-API-Frontend/", // Coloque aqui o nome exato do seu repo no Github
+  base: '/Document-API-Frontend/', // Coloque aqui o nome exato do seu repo no Github
 })
