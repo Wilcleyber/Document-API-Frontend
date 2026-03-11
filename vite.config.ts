9d2dd6eda3c76689b6640790d72d52ac8bf0dcb8
unknown <wilcleyber2@gmail.com>
2026-03-11 09:48:14 -0300

    corrigido vercel procurando em local errado!

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Document-API-Frontend/", // 👈 nome exato do repositório
+  base: "/", // 👈 Na Vercel, o site fica na raiz, então usamos "/"
+  build: {
+    outDir: 'dist', // Pasta padrão de saída
+  }
 });
